364ae1e6d824854f9407d2dab9df7f1d7e477c88
Prabeshpd <Prabeshpd@users.noreply.github.com>
2023-05-21 02:30:37 +0000

    Prettified Code!

--- src/services/home.ts
+++ src/services/home.ts
@@ -1,16 +1,16 @@
 interface AppInfoInterface {
-    name: string;
-    version: string;
-  }
-  
-  /**
-   * Returns the application information.
-   *
-   * @returns {Promise<AppInfoInterface>}
-   */
-  export async function getAppInfo(): Promise<AppInfoInterface> {
-    return {
-      name: process.env.npm_package_name || '',
-      version: process.env.npm_package_version || ''
-    };
-  }
+  name: string;
+  version: string;
+}
+
+/**
+ * Returns the application information.
+ *
+ * @returns {Promise<AppInfoInterface>}
+ */
+export async function getAppInfo(): Promise<AppInfoInterface> {
+  return {
+    name: process.env.npm_package_name || '',
+    version: process.env.npm_package_version || ''
+  };
+}
