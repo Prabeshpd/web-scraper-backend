9c834e06d3e586eddc3562db7b0c95f9b50a85be
Prabeshpd <Prabeshpd@users.noreply.github.com>
2023-05-30 11:11:45 +0000

    Prettified Code!

--- test/api/user.spec.ts
+++ test/api/user.spec.ts
@@ -6,28 +6,28 @@ import { getTestDatabaseConnection } from '../../src/utils/testConnection';
 
 describe('User Creation test', () => {
   // if (!process.env.CI) {
-    let connection: any;
-    before(async () => {
-      connection = getTestDatabaseConnection();
-    });
+  let connection: any;
+  before(async () => {
+    connection = getTestDatabaseConnection();
+  });
 
-    it('should create the user', (done) => {
-      request(app)
-        .post('/api/v1/users')
-        .send({ email: 'random_jude@gmail.com', password: 'random@123', name: 'random jude' })
-        .end((err, res) => {
-          expect(res.statusCode).to.be.equal(200);
-          expect(res.body.name).to.be.equal('random jude');
-          expect(res.body).to.not.haveOwnProperty('password');
-          expect(res.body.email).to.be.equal('random_jude@gmail.com');
-          done();
-        });
-    });
+  it('should create the user', (done) => {
+    request(app)
+      .post('/api/v1/users')
+      .send({ email: 'random_jude@gmail.com', password: 'random@123', name: 'random jude' })
+      .end((err, res) => {
+        expect(res.statusCode).to.be.equal(200);
+        expect(res.body.name).to.be.equal('random jude');
+        expect(res.body).to.not.haveOwnProperty('password');
+        expect(res.body.email).to.be.equal('random_jude@gmail.com');
+        done();
+      });
+  });
 
-    after(async () => {
-      await connection.delete().from('users');
+  after(async () => {
+    await connection.delete().from('users');
 
-      server.close();
-    });
+    server.close();
+  });
   // }
 });
